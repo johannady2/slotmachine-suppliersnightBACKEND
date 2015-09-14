9f32e1f1031fee404bccb7b911502b18e4a24a8f
johannady2 <johannady2@gmail.com>
2015-09-14 15:11:51 +0800

    now supports multiple terminals 2

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 </div><!--//<div class="container">-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
+<script src="js/jquery-2.1.3.min.js"></script>
 <script src="js/custom.js"></script>
 
 
--- process_update_plays_left_field.php
+++ process_update_plays_left_field.php
@@ -3,13 +3,18 @@ header("Access-Control-Allow-Origin: *");
 include_once'connect.php';
 
 
+
+
+
+$store_date_start = date('Y-m-d', strtotime($row["store_datetime_start"]));
+
 $playsleft = $_POST['playsleft'];
 $id_preference = 1;
 
 if(isset($_POST['playsleft']))
 {
-	$stmt = $pdo->prepare ("UPDATE lucky8_admin_preference SET plays_left = :plays_left  WHERE 	id_preference=:id_preference");
-		$stmt -> bindParam(':plays_left', $playsleft);
+	$stmt = $pdo->prepare ("UPDATE lucky8_admin_preference SET plays_left = (plays_left - 1)  WHERE 	id_preference=:id_preference");
+		//$stmt -> bindParam(':plays_left', $playsleft);
 		$stmt -> bindParam(':id_preference', $id_preference);
 		$stmt -> execute();
 		
