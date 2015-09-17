d7d78a801e1a5bef3d9d0990d313e87fb08c7c01
johannady2 <johannady2@gmail.com>
2015-09-17 23:28:36 +0800

    transaction

--- process_update_plays_left_field.php
+++ process_update_plays_left_field.php
@@ -3,21 +3,68 @@ header("Access-Control-Allow-Origin: *");
 include_once'connect.php';
 
 
+$pdo->beginTransaction();
+
+//select
+$sth = $pdo->prepare("SELECT plays_left FROM lucky8_admin_preference  FOR UPDATE;");
+$sth->execute();
+$row = $sth->fetch();
+$playskkk = $row['plays_left'];			
+$echothis ='';
+		
+		if($playskkk > 0)
+		{	
+	
+			//update 
+			$id_preference = 1;
+			$sth2 = $pdo->prepare("UPDATE lucky8_admin_preference SET plays_left = (plays_left - 1)  WHERE 	id_preference=:id_preference");
+			$sth2-> bindParam(':id_preference', $id_preference);
+			$sth2-> execute();
+
+			$pdo->commit();
+			$echothis ='success';
+		}
+		else
+		{
+			 $pdo->rollBack();
+			 $echothis ='fail';
+		}
+
+echo $echothis;
+?>
 
 
+<?php
+/*NO TRANSACTION VERSION.. still works because of "FRO UPDATE" but with transaction is better*/
+/* 
+header("Access-Control-Allow-Origin: *");
+include_once'connect.php';
 
-$store_date_start = date('Y-m-d', strtotime($row["store_datetime_start"]));
-
-$playsleft = $_POST['playsleft'];
 $id_preference = 1;
 
-if(isset($_POST['playsleft']))
-{
-	$stmt = $pdo->prepare ("UPDATE lucky8_admin_preference SET plays_left = (plays_left - 1)  WHERE 	id_preference=:id_preference");
-		//$stmt -> bindParam(':plays_left', $playsleft);
-		$stmt -> bindParam(':id_preference', $id_preference);
-		$stmt -> execute();
+
+$result = $pdo->prepare("SELECT plays_left FROM lucky8_admin_preference  FOR UPDATE;");
+$result->execute();
+$row = $result->fetch();
+$playskkk = $row['plays_left'];
+
 		
-	echo 'success';
-}
+$echothis ='';
+		
+		if($playskkk > 0)
+		{
+			$stmt = $pdo->prepare ("UPDATE lucky8_admin_preference SET plays_left = (plays_left - 1)  WHERE 	id_preference=:id_preference");
+			
+				$stmt -> bindParam(':id_preference', $id_preference);
+				$stmt -> execute();
+			
+			$echothis ='success';
+		}
+		else
+		{
+			$echothis ='fail';
+		}
+
+echo $echothis;
+*/
 ?>
--- process_update_wins_left_field.php
+++ process_update_wins_left_field.php
@@ -3,12 +3,19 @@ header("Access-Control-Allow-Origin: *");
 include_once'connect.php';
 
 
+$pdo->beginTransaction();
 
-$wins_left = $_POST['wins_left'];
-$id_preference = 1;
+//select
+$sth = $pdo->prepare("SELECT wins_left FROM lucky8_admin_preference  FOR UPDATE;");
+$sth->execute();
+$row = $sth->fetch();
+$winskkk = $row['wins_left'];			
+$echothis ='';
 
-if(isset($_POST['wins_left']))
+if($winskkk > 0)
 {
+	$id_preference = 1;
+	
 	$stmt = $pdo->prepare ("UPDATE lucky8_admin_preference SET wins_left=  (wins_left -1) WHERE 	id_preference=:id_preference");
 
 		//$stmt -> bindParam(':wins_left', $wins_left);
@@ -16,6 +23,14 @@ if(isset($_POST['wins_left']))
 		$stmt -> bindParam(':id_preference', $id_preference);
 		$stmt -> execute();
 		
-	echo 'success';
+	$pdo->commit();
+	$echothis ='success';
 }
+else
+{
+	$echothis ='fail';
+	$pdo->rollBack();
+}
+
+echo $echothis;
 ?>
